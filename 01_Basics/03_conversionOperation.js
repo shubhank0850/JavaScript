a96e6ea44adfadea0f085314985994f9456cc304
shubhank0850 <117857265+shubhank0850@users.noreply.github.com>
2025-04-14 17:39:41 +0000

    string conversion

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -24,9 +24,34 @@ true => 1; false => 0
 let isLoggedIn = "shubhu"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /* Notes: [Conversoin to Boolean]
 1 => true; 0 => false
 "" => false; "shubhu" => true
 */
+
+/****************** Operations *****************/
+
+let value  = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+//console.log(2**2); /2 to power 2
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " shubhu"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); /12
+// console.log(1 + "2"); /12
+// console.log("1" + "2"); /12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32 [(1+2) + "2"]
