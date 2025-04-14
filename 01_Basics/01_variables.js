05ba832d89d0638c24c5f8fd6806f4bf8448e37e
shubhank0850 <117857265+shubhank0850@users.noreply.github.com>
2025-04-14 15:15:44 +0000

    just a arranged things to make it redable

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,23 @@
+/* Note :
+1. Prefere not to use var
+   because of issue in block scope and functional scope
+
+2. In Js if you declare a variable and does not assign a value to it,
+   then it prints undefined. (Eaxmple: line 15)
+*/
+
+const accountId = 13335
+let accountEmail = "shubhank@google.com"
+var accountPassword = "12345"
+accountCity = "Nagpur"
+let accountState;
+
+// accountId = 2 : changing/updating const variable is not allowed.
+
+accountEmail = "bawankar@gmail.com"
+accountPassword = "54321"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
