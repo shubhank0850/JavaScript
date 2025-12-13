143a65371790ff12e7b5854543ee67c91e38482b
shubhank0850 <117857265+shubhank0850@users.noreply.github.com>
2025-12-13 08:18:23 +0000

    foreach method

---
+++ 02_Basics/08_forEach.js
@@ -0,0 +1,52 @@
+// forEach() = method used to iterate over the elements
+//             of an array and apply a specific function (callback)
+//             to each element
+
+//             arrays.forEach(callback)
+//             element, index, array are provided
+
+// Example 1
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// numbers.forEach(cube);
+// numbers.forEach(display);
+
+// function double(element, index, array){
+//     array[index] = element * 2;
+// }
+
+// function triple(element, index, array){
+//     array[index] = element * 3;
+// }
+
+// function square(element, index, array){
+//     array[index] = Math.pow(element, 2);
+// }
+
+// function cube(element, index, array){
+//     array[index] = Math.pow(element, 3);
+// }
+
+// function display(element){
+//     console.log(element);
+// }
+
+// Example 2
+
+let fruits = ["apple", "orange", "banana", "melon", "guava"];
+
+fruits.forEach(capitalize);
+fruits.forEach(display);
+
+function upperCase(element, index, array){
+    array[index] = element.toUpperCase();
+}
+
+function capitalize(element, index, array){
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
+}
+
+function display(element){
+    console.log(element);
+}
